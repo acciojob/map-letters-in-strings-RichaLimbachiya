35f989d9e261d3c1c743bc6879d325c8e6200de4
RichaLimbachiya <richalimbachiya2004@gmail.com>
2025-05-15 11:00:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+function mapLetters(word) {
+  const letterMap = {};
+
+  for (let i = 0; i < word.length; i++) {
+    const char = word[i];
+
+    if (!letterMap[char]) {
+      letterMap[char] = [];
+    }
+
+    letterMap[char].push(i);
+  }
+
+  return letterMap;
+}
